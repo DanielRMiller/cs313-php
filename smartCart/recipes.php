5f6eabd0e4afc757b2b128af745cf0429d6c51ec
Daniel Miller <d.miller1.work@gmail.com>
2016-05-23 05:24:04 -0600

    Connect to database in correct way for openshift or localhost.

--- smartCart/recipes.php
+++ smartCart/recipes.php
@@ -1,16 +1,32 @@
 <?php
   session_start();
-  if (!$_SESSION['username']) {
-    header("Location: index.php");
-  }
-  try
-  {
-    $db = new PDO('mysql:host=localhost;dbname=smartcart;charset=utf8mb4', 'smartCartApp', 'abc123');
-  }
-  catch (PDOException $ex) 
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die(); 
+  // See if we are on openshift or localhost
+  $dbHost = "";
+  $dbPort = "";
+  $dbUser = "";
+  $dbPassword = "";
+  $dbName = "smartcart";
+  $openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+  // Now check where we are running this
+  if ($openShiftVar === null || $openShiftVar == "") {
+    // We are in the local host
+    $dbHost = 'localhost';
+    $dbPort = '';
+    $dbUser = 'smartCartApp';
+    $dbPassword = 'abc123';
+  } else {
+    // We are on the openshift database!
+    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+    $dbPort = ":" . getenv('OPENSHIFT_MYSQL_DB_PORT');
+    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+    }
+  try {
+    // Connect to the database!
+    $db = new PDO("mysql:host=$dbHost$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  } catch (PDOException $e) {
+    echo "ERROR in connecting to database: " . $e->getMessage();
+    die();
   }
   $stmt = $db->prepare("
     SELECT r.name, r.description 
