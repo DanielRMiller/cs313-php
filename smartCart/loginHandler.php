c68f9535a14431f23868f274d8a950162a560017
Daniel Miller <d.miller1.work@gmail.com>
2016-05-30 20:34:25 -0600

    Add id to session and send back to index instead of login.

--- smartCart/loginHandler.php
+++ smartCart/loginHandler.php
@@ -1,20 +1,23 @@
 <?php
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if ($username == "" || $password == "") { header("Location: login.php"); }
+    if ($username == "" || $password == "") { header("Location: index.php"); }
     include 'dbConnect.php';
     $stmt = $db->prepare("
-        SELECT username, password, first_name, last_name 
+        SELECT id, username, password, first_name, last_name 
         FROM user 
         WHERE username=:username 
-            AND password=:password;");
+            AND password=:password
+        LIMIT 1;
+        ");
     $stmt->execute(array(':username' => $username, ':password' => $password));
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
     if ($rows['password'] == $password) {
         session_start();
+        $_SESSION['id'] = $rows['id'];
         $_SESSION['username'] = $username;
         $_SESSION['first_name'] = $rows['first_name'];
         $_SESSION['last_name'] = $rows['last_name'];
         header("Location: recipes.php");
-    } else { header("Location: login.php"); }
+    } else { header("Location: index.php"); }
 ?>
